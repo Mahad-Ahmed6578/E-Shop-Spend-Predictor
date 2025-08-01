6c3d271ed422bedd876edf2405126447fe1e86bb
Muhammad-Mahad6611 <137657632+Mahad-Ahmed6578@users.noreply.github.com>
2025-08-01 05:48:39 +0500

    utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,1 @@
-def evaluate_prediction(actual, predicted):
-    error = abs(actual - predicted)
-    percentage_error = (error / actual) * 100
-    result = {
-        'Actual Value': actual,
-        'Predicted Value': predicted,
-        'Absolute Error': round(error, 2),
-        'Percentage Error': round(percentage_error, 2)
-    }
-    return result
+
